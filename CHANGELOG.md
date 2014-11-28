4c6d16f04800cdc9d8479dc12a1d759b4d355f87
James Smith <james@floppy.org.uk>
2014-11-28 09:40:57 +0000

    Change github diff selector
    Resolves #6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.0.2
+
+Updatesd for changes to github diff CSS selectors
+
 ## 1.0.1
 
 Fixed bug where CSV diffs would not display at all
--- csvhub.js
+++ csvhub.js
@@ -8,7 +8,7 @@ for (var f = 0; f < files.length; f++) {
 
 
     // Get all diff lines
-    lines = files[f].querySelectorAll(".diff-line-code");
+    lines = files[f].querySelectorAll(".blob-code");
 
     // Get data
     var old_data = []
