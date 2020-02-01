db49eb625d51c6e6e1c29bd47a37796dbe166d4c
weiyang <weiyang.ones@gmail.com>
2020-02-01 12:07:36 +0800

    [csvhub.js] Fix code marker
    Signed-off-by: weiyang <weiyang.ones@gmail.com>

--- csvhub.js
+++ csvhub.js
@@ -1,48 +1,45 @@
 // find all files in the page
 files = $("div#files.diff-view .file");
 for (var f = 0; f < files.length; f++) {
-
   // check if this is a CSV file
-  filename = files[f].querySelector("div[data-path]").getAttribute('data-path');
-  if (filename.match(".*\.csv")) {
-
-
+  filename = files[f].querySelector("div[data-path]").getAttribute("data-path");
+  if (filename.match(".*.csv")) {
     // Get all diff lines
-    lines = files[f].querySelectorAll(".blob-code-inner");
+    lines = files[f].querySelectorAll(".blob-code-marker");
 
     // Get data
-    var old_data = []
-    var new_data = []
+    var old_data = [];
+    var new_data = [];
 
     for (var l = 0; l < lines.length; l++) {
-
       // Parse data from line
+      code_marker = lines[l].getAttribute("data-code-marker");
       line = lines[l].textContent;
       data = $.csv.toArray(line.substr(1).trim());
+      console.log(data);
 
       // Line has been added
-      if (line.indexOf("+") == 0) {
+      if (code_marker == "+") {
         new_data.push(data);
       }
 
       // Line has been removed
-      if (line.indexOf("-") == 0) {
+      if (code_marker == "-") {
         old_data.push(data);
       }
 
       // Line has not changed
-      if (line.indexOf(" ") == 0) {
+      if (code_marker == " ") {
         new_data.push(data);
         old_data.push(data);
       }
-
     }
 
     // Parse CSV
     var old_table = new daff.TableView(old_data);
     var new_table = new daff.TableView(new_data);
 
-    var alignment = daff.compareTables(old_table,new_table).align();
+    var alignment = daff.compareTables(old_table, new_table).align();
 
     var data_diff = [];
     var table_diff = new daff.TableView(data_diff);
@@ -51,15 +48,14 @@ for (var f = 0; f < files.length; f++) {
     flags.show_unchanged = true;
     flags.show_unchanged_columns = true;
     flags.always_show_header = false;
-    var highlighter = new daff.TableDiff(alignment,flags);
+    var highlighter = new daff.TableDiff(alignment, flags);
     highlighter.hilite(table_diff);
 
     var diff2html = new daff.DiffRender();
     diff2html.render(table_diff);
-    diff_html = diff2html.html()
+    diff_html = diff2html.html();
 
-    files[f].querySelector("div.data").innerHTML = "<div class='csvhub-diff'>"+diff_html+"</div>";
-
+    files[f].querySelector("div.data").innerHTML =
+      "<div class='csvhub-diff'>" + diff_html + "</div>";
   }
-
 }
